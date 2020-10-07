42f14d9a603cad5d4ded321f58ae30dd9e31e608
Minseo Kim <minseokim@Minseos-MacBook-Pro-2.local>
2020-10-07 13:46:53 +0900

    Add simple java method and junit test

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,8 @@
 package com.mycompany.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,24 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public static class Human {
+
+        private String name;
+        private int age;
+
+        public Human(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String greet(String str) {
+            return str +  ", " + this.name;
+        }
+
+        public boolean isAgeCorrect(int age) {
+            return this.age == age;
+        }
     }
 }
--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,12 @@
 package com.mycompany.app;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.mycompany.app.App.Human;
 
 /**
  * Unit test for simple App.
@@ -17,4 +21,17 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    @Test
+     public void greet() {
+         Human Alex = new Human("Alex", 29);
+        assertEquals("Hey there, Alex", Alex.greet("Hey there"));
+     }
+    
+     @Test
+     public void isAgeCorrect() {
+         Human Alex = new Human("Alex", 29);
+         assertNotNull(Alex.isAgeCorrect(28));
+        assertEquals(true, Alex.isAgeCorrect(29));
+     }
 }
